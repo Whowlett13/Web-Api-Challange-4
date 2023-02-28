5b382b4590a1cda6c9a7a93d47ed7314118e1ed8
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-02-28 00:00:36 -0500

    adding changes to html and css

---
+++ script.js
@@ -0,0 +1,20 @@
+var startButton = document.getElementById("StartButton");
+var time = 60;
+var questionContainer = document.getElementById();
+var answerA = document.getElementById();
+var answerB = document.getElementById();
+var answerC = document.getElementById();
+var answerD = document.getElementById();
+
+var score = 0;
+
+let i = 0;
+
+
+
+startButton.addEventListener("click", function(){
+
+
+
+    
+})
