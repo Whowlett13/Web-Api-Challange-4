266bfc19036aa697a6a7fc16a282c844daa6c4e2
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-02-28 00:08:09 -0500

    adding changes to javascript

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var startButton = document.getElementById("StartButton");
+var startButton = document.getElementById("startButton");
 var time = 60;
 var questionContainer = document.getElementById();
 var answerA = document.getElementById();
 var answerB = document.getElementById();
 var answerC = document.getElementById();
 var answerD = document.getElementById();
-
+var countdownTimer = document.getElementById("scoreCounter");
 var score = 0;
 
 let i = 0;
+
+
+var Questions
+
+
 
 
 
@@ -16,5 +21,5 @@ startButton.addEventListener("click", function(){
 
 
 
-    
+
 })
