b0cc02fd47bc3bee8d87d9772bc829c626985e34
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-02-28 17:33:33 -0500

    commiting css styling, html & unfinished javascrpit

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
-var startButton = document.getElementById("startButton");
-var time = 60;
-var questionContainer = document.getElementById();
-var answerA = document.getElementById();
-var answerB = document.getElementById();
-var answerC = document.getElementById();
-var answerD = document.getElementById();
-var countdownTimer = document.getElementById("scoreCounter");
-var score = 0;
 
-let i = 0;
+var questions = [
+    {
+        questionText: "Commonly used data types DO NOT include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts",
+    },
+    {
+        questionText: "Arrays in JavaScript can be used to store ______.",
+        options: [
+            "1. Numbers & Strings",
+            "2. Other Arrays",
+            "3. Booleans",
+            "4. All Of The Above",
+        ],
+        answer: "4. All of the above",
+    },
+    {
+        questionText:
+            "String values must be enclosed within _____ when being assigned to variables.",
+        options: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
+        answer: "3. Quotes",
+    },
+    {
+        questionText:
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log",
+        ],
+        answer: "4. console.log",
+    },
+    {
+        questionText:
+            "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+        options: ["1. break", "2. stop", "3. halt", "4. exit"],
+        answer: "1. break",
+    },
+];
 
 
-var Questions
+var startCard = document.querySelector("#start-card");
+var questionCard = document.querySelector("#question-card");
+var scoreCard = document.querySelector("#score-card");
+var leaderboardCard = document.querySelector("#leaderboard-card");
+
+//hide all cards
+function hideCards() {
+    startCard.setAttribute("hidden", true);
+    questionCard.setAttribute("hidden", true);
+    scoreCard.setAttribute("hidden", true);
+    leaderboardCard.setAttribute("hidden", true);
+}
+
+var resultDiv = document.querySelector("#result-div");
+var resultText = document.querySelector("#result-text");
 
 
+function hideResultText() {
+    resultDiv.style.display = "none";
+}
 
 
+var intervalID;
+var time;
+var currentQuestion;
 
-startButton.addEventListener("click", function(){
+document.querySelector("#start-button").addEventListener("click", startQuiz);
 
-
-
-
-})
