6f419ef741b221df17398258aa3a494a4f503782
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-03-01 11:40:53 -0500

    commiting javascrpit

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ var questions = [
             "3. Booleans",
             "4. All Of The Above",
         ],
-        answer: "4. All of the above",
+        answer: "4. All Of The Above",
     },
     {
         questionText:
-            "String values must be enclosed within _____ when being assigned to variables.",
+            "String Values Must Be Enclosed Within _____ When Being assigned to variables.",
         options: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parentheses"],
         answer: "3. Quotes",
     },
@@ -35,8 +35,8 @@ var questions = [
     {
         questionText:
             "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
-        options: ["1. break", "2. stop", "3. halt", "4. exit"],
-        answer: "1. break",
+        options: ["1. Break", "2. Stop", "3. Back", "4. Exit"],
+        answer: "1. Break",
     },
 ];
 
@@ -69,3 +69,216 @@ var currentQuestion;
 
 document.querySelector("#start-button").addEventListener("click", startQuiz);
 
+function startQuiz() {
+    
+    hideCards();
+    questionCard.removeAttribute("hidden");
+
+    /
+    currentQuestion = 0;
+    displayQuestion();
+
+    
+    time = questions.length * 10;
+
+    
+    intervalID = setInterval(countdown, 1000);
+
+    
+    displayTime();
+}
+
+
+function countdown() {
+    time--;
+    displayTime();
+    if (time < 1) {
+        endQuiz();
+    }
+}
+
+
+var timeDisplay = document.querySelector("#time");
+function displayTime() {
+    timeDisplay.textContent = time;
+}
+
+
+function displayQuestion() {
+    let question = questions[currentQuestion];
+    let options = question.options;
+
+    let h2QuestionElement = document.querySelector("#question-text");
+    h2QuestionElement.textContent = question.questionText;
+
+    for (let i = 0; i < options.length; i++) {
+        let option = options[i];
+        let optionButton = document.querySelector("#option" + i);
+        optionButton.textContent = option;
+    }
+}
+
+
+document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+
+
+function optionIsCorrect(optionButton) {
+    return optionButton.textContent === questions[currentQuestion].answer;
+}
+
+
+function checkAnswer(eventObject) {
+    let optionButton = eventObject.target;
+    resultDiv.style.display = "block";
+    if (optionIsCorrect(optionButton)) {
+        resultText.textContent = "Correct!";
+        setTimeout(hideResultText, 1000);
+    } else {
+        resultText.textContent = "Incorrect!";
+        setTimeout(hideResultText, 1000);
+        if (time >= 10) {
+            time = time - 10;
+            displayTime();
+        } else {
+
+            time = 0;
+            displayTime();
+            endQuiz();
+        }
+    }
+
+    
+    currentQuestion++;
+
+    if (currentQuestion < questions.length) {
+        displayQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+
+const score = document.querySelector("#score");
+
+
+function endQuiz() {
+    clearInterval(intervalID);
+    hideCards();
+    scoreCard.removeAttribute("hidden");
+    score.textContent = time;
+}
+
+const submitButton = document.querySelector("#submit-button");
+const inputElement = document.querySelector("#initials");
+
+
+submitButton.addEventListener("click", storeScore);
+
+function storeScore(event) {
+    
+    event.preventDefault();
+
+    
+    if (!inputElement.value) {
+        alert("Please enter your initials before pressing submit!");
+        return;
+    }
+
+    
+    let leaderboardItem = {
+        initials: inputElement.value,
+        score: time,
+    };
+
+    updateStoredLeaderboard(leaderboardItem);
+
+    
+    hideCards();
+    leaderboardCard.removeAttribute("hidden");
+
+    renderLeaderboard();
+}
+
+
+function updateStoredLeaderboard(leaderboardItem) {
+    let leaderboardArray = getLeaderboard();
+    //append new leaderboard item to leaderboard array
+    leaderboardArray.push(leaderboardItem);
+    localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
+}
+
+
+function getLeaderboard() {
+    let storedLeaderboard = localStorage.getItem("leaderboardArray");
+    if (storedLeaderboard !== null) {
+        let leaderboardArray = JSON.parse(storedLeaderboard);
+        return leaderboardArray;
+    } else {
+        leaderboardArray = [];
+    }
+    return leaderboardArray;
+}
+
+
+function renderLeaderboard() {
+    let sortedLeaderboardArray = sortLeaderboard();
+    const highscoreList = document.querySelector("#highscore-list");
+    highscoreList.innerHTML = "";
+    for (let i = 0; i < sortedLeaderboardArray.length; i++) {
+        let leaderboardEntry = sortedLeaderboardArray[i];
+        let newListItem = document.createElement("li");
+        newListItem.textContent =
+            leaderboardEntry.initials + " - " + leaderboardEntry.score;
+        highscoreList.append(newListItem);
+    }
+}
+
+
+function sortLeaderboard() {
+    let leaderboardArray = getLeaderboard();
+    if (!leaderboardArray) {
+        return;
+    }
+
+    leaderboardArray.sort(function (a, b) {
+        return b.score - a.score;
+    });
+    return leaderboardArray;
+}
+
+const clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", clearHighscores);
+
+//clear local storage and display empty leaderboard
+function clearHighscores() {
+    localStorage.clear();
+    renderLeaderboard();
+}
+
+const backButton = document.querySelector("#back-button");
+backButton.addEventListener("click", returnToStart);
+
+//Hide leaderboard card show start card
+function returnToStart() {
+    hideCards();
+    startCard.removeAttribute("hidden");
+}
+
+
+const leaderboardLink = document.querySelector("#leaderboard-link");
+leaderboardLink.addEventListener("click", showLeaderboard);
+
+function showLeaderboard() {
+    hideCards();
+    leaderboardCard.removeAttribute("hidden");
+
+    
+    clearInterval(intervalID);
+
+    
+    time = undefined;
+    displayTime();
+
+    
+    renderLeaderboard();
+}
